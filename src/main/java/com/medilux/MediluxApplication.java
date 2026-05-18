0deb97440b0e95a4095799484444e0b7317e0503
IT25103650 <it25103650@my.sliit.lk>
2026-05-18 13:24:05 +0530

    Add common configuration files to main

---
+++ src/main/java/com/medilux/MediluxApplication.java
@@ -0,0 +1,12 @@
+package com.medilux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MediluxApplication.class, args);
+    }
+}
